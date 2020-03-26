a7e0bb4f5b9c289d289368c177b011d8ad173f20
João Paulo da Mata Mendes <jp_cbc@hotmail.com>
2020-03-26 01:18:13 -0300

    Preparando a tela de login

---
+++ src/main/java/br/com/jopaulo/jopaulofood/infrastructure/web/controller/LoginController.java
@@ -0,0 +1,13 @@
+package br.com.jopaulo.jopaulofood.infrastructure.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+	
+	@GetMapping(path = {"/login", "/"})
+	public String login() {
+		return "login";
+	}
+}

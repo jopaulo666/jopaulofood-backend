113b866c8ad3864c0ab838048b3fc37da0f29528
João Paulo da Mata Mendes <jp_cbc@hotmail.com>
2020-03-26 00:31:50 -0300

    Criando o projeto e importando as telas do protótipo para dentro do projeto

---
+++ src/main/java/br/com/jopaulo/jopaulofood/JopauloFoodApplication.java
@@ -0,0 +1,13 @@
+package br.com.jopaulo.jopaulofood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JopauloFoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JopauloFoodApplication.class, args);
+	}
+
+}

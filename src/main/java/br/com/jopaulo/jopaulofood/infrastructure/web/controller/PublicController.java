730990fb3300fe95b5bdd0dfd7947991c224102e
João Paulo da Mata Mendes <jp_cbc@hotmail.com>
2020-03-29 22:18:44 -0300

    Iniciando a tela de cadastro de cliente

---
+++ src/main/java/br/com/jopaulo/jopaulofood/infrastructure/web/controller/PublicController.java
@@ -0,0 +1,19 @@
+package br.com.jopaulo.jopaulofood.infrastructure.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import br.com.jopaulo.jopaulofood.domain.cliente.Cliente;
+
+@Controller
+@RequestMapping(path = "/public")
+public class PublicController {
+
+	@GetMapping("/cliente/new")
+	public String newCliente(Model model) {		
+//		model.addAttribute("cliente", new Cliente());		
+		return "cliente-cadastro";
+	}
+}

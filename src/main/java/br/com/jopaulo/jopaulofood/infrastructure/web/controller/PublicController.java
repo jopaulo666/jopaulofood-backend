0881b82a086d0aa4b1f7fa1853643c0461c73b2c
João Paulo da Mata Mendes <jp_cbc@hotmail.com>
2020-04-09 01:07:03 -0300

    Mostrando mensagem de erro de validação

--- src/main/java/br/com/jopaulo/jopaulofood/infrastructure/web/controller/PublicController.java
+++ src/main/java/br/com/jopaulo/jopaulofood/infrastructure/web/controller/PublicController.java
@@ -1,8 +1,11 @@
 package br.com.jopaulo.jopaulofood.infrastructure.web.controller;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -26,8 +29,14 @@ public class PublicController {
 	}
 	
 	@PostMapping(path = "/cliente/save")
-	public String saveCliente(@ModelAttribute("cliente") Cliente cliente) {
-		clienteService.saveCliente(cliente);
+	public String saveCliente(@ModelAttribute("cliente") @Valid Cliente cliente, Errors errors, Model model) {
+		
+		if (!errors.hasErrors()) {
+			clienteService.saveCliente(cliente);
+			model.addAttribute("msg", "Cliente salvo com sucesso");
+		}
+		
+		ControllerHelper.setEditModel(model, false);
 		return "cliente-cadastro";
 	}
 }

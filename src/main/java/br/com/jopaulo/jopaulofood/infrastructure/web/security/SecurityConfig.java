c48a61dc703b4c8680a37eb9a363643142445953
João Paulo da Mata Mendes <jp_cbc@hotmail.com>
2020-05-03 12:15:38 -0300

    Configurando a segurança da aplicação - Parte 3

--- src/main/java/br/com/jopaulo/jopaulofood/infrastructure/web/security/SecurityConfig.java
+++ src/main/java/br/com/jopaulo/jopaulofood/infrastructure/web/security/SecurityConfig.java
@@ -1,11 +1,18 @@
 package br.com.jopaulo.jopaulofood.infrastructure.web.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	@Bean
+	public AuthenticationSuccessHandler authenticationSuccessHandler() {
+		return new AuthenticationSuccessHandlerImpl();
+	}
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
@@ -18,8 +25,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 			.and()
 			.formLogin()
 				.loginPage("/login")
-				.failureUrl("login-error")
-//				.successHandler(null)
+				.failureUrl("/login-error")
+				.successHandler(authenticationSuccessHandler())
 				.permitAll()
 			.and()
 				.logout().logoutUrl("/logout")
